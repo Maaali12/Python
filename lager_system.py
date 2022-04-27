1d0d5b17e4adc8634e4da857ff7121d4d949715b
Maaali12 <pmalihd@gmail.com>
2022-04-27 14:46:31 +0200

    Lager System

--- lager_system.py
+++ lager_system.py
@@ -1,20 +1,36 @@
 import sys
 
-Bestand = {"Bananen": 1, "Äpfel": 2, "Salz": 99, "Ananas": 28, } #Liste mit vorhandenen gegenständen
-paswort = input("Passwort bitte eingeben:")
+Bestand = {"Bananen": 1, "Äpfel": 2, "Salz": 99, "Ananas": 28,  } 
 
-def error_ausgabe():
+key = "1205"
+
+def fehler_ausgabe():
     print("Fehler: Falsche eingabe!")
     print("Achte bitte auf deine Rechtschreibung!")
-    
-while True:
-    if paswort == "1234":
-        print("Passwort korrekt.")
+
+def auswahl_möglichkeit():
+    print("[1.] Lager bestand abfragen.")
+    print("[2.] Lager bestand bearbeiten.")
+
+def passowrt_abfrage():
+    while True:
+        if paswort == key:
+            print("Passwort korrekt.")
+            break
     else:
         print("Paswort Falsch!")
         quit()
 
+
+
+auswahl_möglichkeit()
+wahl = input("Wähle auswahlmöglichkeit aus.")
+
 while True:
-    print("Bitte Produkt eingeben:")  #Ausgabe
-    index = input()  #Eingabe
-    print("Es sind noch:", Bestand[index], "Stück", "vorhanden.")  #Ausgabe
+    if wahl == "1":
+        passwort_abfrage()
+    else:
+        if wahl == "2":
+        passwort_abfrage()
+    else:
+        fehler_ausgabe()
