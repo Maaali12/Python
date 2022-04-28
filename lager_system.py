7a4f87dbdac695b106266ab219565f956ba351a5
Maaali12 <pmalihd@gmail.com>
2022-04-28 10:13:32 +0200

    Lager System

--- lager_system.py
+++ lager_system.py
@@ -1,3 +1,4 @@
+from operator import index
 import sys
 
 Bestand = {"Bananen": 1, "Äpfel": 2, "Salz": 99, "Ananas": 28,  } 
@@ -13,24 +14,43 @@ def auswahl_möglichkeit():
     print("[2.] Lager bestand bearbeiten.")
 
 def passowrt_abfrage():
+    passwort = input("Bitte Passwort zum Fortfahren eingeben: ")
     while True:
-        if paswort == key:
+        if passwort == key:
             print("Passwort korrekt.")
             break
-    else:
-        print("Paswort Falsch!")
-        quit()
+        else:
+            print("Paswort Falsch!")
+            quit()
+
+def lager_bestand():
+    while True:
+        print("Bitte Produkt eingeben: ")
+        index = input()
+        print("Es sind noch:", Bestand[index], "Stück vorhanden.")
+
+def lager_bearbeiten():
+    print("Dieser befehl ist noch in Arbeit!")
+    auswahl_möglichkeit()
+    auswahl_möglichkeit2()
+
+def auswahl_möglichkeit2():
+    wahl = input("Wähle auswahlmöglichkeit aus: ")
+    while True:
+        if wahl == "1":
+            print("Lager bestand wird abgefragt")
+            passowrt_abfrage()
+            lager_bestand()
+            break
+        elif wahl == "2":
+            passowrt_abfrage()
+            lager_bearbeiten()
+            break 
+        else:
+            fehler_ausgabe()
+            return
 
 
 
 auswahl_möglichkeit()
-wahl = input("Wähle auswahlmöglichkeit aus.")
-
-while True:
-    if wahl == "1":
-        passwort_abfrage()
-    else:
-        if wahl == "2":
-        passwort_abfrage()
-    else:
-        fehler_ausgabe()
+auswahl_möglichkeit2()
