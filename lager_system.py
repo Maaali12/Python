4a719b93e9184c5b27cf999f976679b7dc5c5c2b
Maaali12 <pmalihd@gmail.com>
2022-05-02 14:59:39 +0200

    Datenbank hinzugefügt

--- lager_system.py
+++ lager_system.py
@@ -1,14 +1,17 @@
 from operator import index
 import sys
+import sqlite3
 
-Bestand = {"Bananen": 1, "Äpfel": 2, "Salz": 99, "Ananas": 28,  } 
+Bestand = {"Bananen": 12, "Äpfel": 29, "Salz": 8, "Ananas": 64,  } 
 
 key = "1205"
 
 def fehler_ausgabe():
     print("Fehler: Falsche eingabe!")
     print("Achte bitte auf deine Rechtschreibung!")
-
+    auswahl_möglichkeit()
+    auswahl_möglichkeit2()
+    
 def auswahl_möglichkeit():
     print("[1.] Lager bestand abfragen.")
     print("[2.] Lager bestand bearbeiten.")
@@ -30,7 +33,10 @@ def lager_bestand():
         print("Es sind noch:", Bestand[index], "Stück vorhanden.")
 
 def lager_bearbeiten():
+    print("")
+    print("!! ACHTUNG !!")
     print("Dieser befehl ist noch in Arbeit!")
+    print("")
     auswahl_möglichkeit()
     auswahl_möglichkeit2()
 
@@ -41,15 +47,12 @@ def auswahl_möglichkeit2():
             print("Lager bestand wird abgefragt")
             passowrt_abfrage()
             lager_bestand()
-            break
         elif wahl == "2":
             passowrt_abfrage()
             lager_bearbeiten()
-            break 
         else:
             fehler_ausgabe()
             return
-
 
 
 auswahl_möglichkeit()
